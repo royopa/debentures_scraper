48014b01cd339ab064aeb882fb758483e4ce356a
Rodrigo Prado de Jesus <royopa@gmail.com>
2020-09-28 12:19:18 -0300

    ajustado data final download para pegar historico

--- main.py
+++ main.py
@@ -1,50 +1,58 @@
 # -*- coding: utf-8 -*-
 import csv
-import time
 import os
-import wget
-from tqdm import tqdm
+import time
+from datetime import datetime
+
 import requests
 from bs4 import BeautifulSoup
 
+import utils
+
 
 def get_links():
-    url_base = 'http://www.debentures.com.br/exploreosnd/consultaadados/emissoesdedebentures/'
-    url = url_base+'puhistorico_f.asp'
+    url_base = 'http://www.debentures.com.br'
+    url_pu = f'{url_base}/exploreosnd/consultaadados/emissoesdedebentures/'
+    url = url_pu+'puhistorico_f.asp'
     res = requests.get(url)
 
     while res.status_code != 200:
         res = requests.get(url)
 
-    soup = BeautifulSoup(res.text,"html.parser")
-    select = soup.find("select", {"name":"ativo"})
+    soup = BeautifulSoup(res.text, "html.parser")
+    select = soup.find("select", {"name": "ativo"})
 
     urls = []
     for option in select.find_all('option'):
         ativo = option['value'].strip()
-        
+
         if len(ativo) < 6:
             continue
-        
+
         ativo = ativo.replace(' ', '+')
-        url_download = url_base + 'puhistorico_e.asp?op_exc=False&dt_ini=&dt_fim=&Submit.x=34&Submit.y=13&ativo='+ativo+'++++'
-        urls.append({'ativo': ativo,'url':url_download})
+
+        today = datetime.today()
+
+        url_compl = '/exploreosnd/consultaadados/emissoesdedebentures/'
+        url = f'{url_base}{url_compl}'
+        url = f'{url}puhistorico_e.asp?'
+        url = f'{url}op_exc=False&dt_ini=&Submit.x=34&Submit.y=13'
+        url = f"{url}&dt_fim={today.strftime('%d/%m/%Y')}&ativo={ativo}++++"
+        print(url)
+        urls.append({'ativo': ativo, 'url': url})
 
     return urls
 
 
 def main():
-    directory = 'downloads'
-    if not os.path.exists(directory):
-        os.makedirs(directory)
+    utils.prepare_download_folder('downloads')
+
     urls = get_links()
     for url in urls:
-        print(' Baixando arquivo do ativo', url['ativo'])
         name_file = url['ativo']+'.csv'
-        print(name_file)
         path_file = os.path.join('downloads', name_file)
-        if not os.path.exists(path_file):
-            wget.download(url['url'], path_file)
+        print(' Baixando arquivo do ativo', url['ativo'], name_file)
+        utils.download(url['url'], None, path_file)
 
 
 if __name__ == '__main__':
